a2828184a3b09fc6e2a59a54577d1fce5bfca794
FelipeEmbersisc <felipe.embersisc@gmail.com>
2021-01-08 14:09:46 -0300

    users controller finished

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,3 @@
-const { json } = require('express')
 const knex = require('../database')
 
 module.exports = {
@@ -6,7 +5,7 @@ module.exports = {
   async index(req, res, next) {
     try {
       const results = await knex('users')
-        .where('deleted_at', null)
+      .where('deleted_at', null)
 
       return res.json(results)
     } catch (error) {
@@ -24,6 +23,33 @@ module.exports = {
       })
 
       return res.status(201).send()
+    } catch (error) {
+      next(error)
+    }
+  },
+  async update(req, res, next) {
+    try {
+      const { name, username, email } = req.body
+      const { id } = req.params
+
+      await knex('users')
+      .update({ name, username, email })
+      .where({ id })
+
+      return res.send()
+    } catch (error) {
+      next(error)
+    }
+  },
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params
+
+      await knex('users')
+      .update('deleted_at', new Date())
+      .where({ id })
+
+      return res.send()
     } catch (error) {
       next(error)
     }
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,7 @@ const UserController = require('./controllers/UserController')
 routes
   .get('/users', UserController.index)
   .post('/users', UserController.create)
+  .put('/users/:id', UserController.update)
+  .delete('/users/:id', UserController.delete)
 
 module.exports = routes
